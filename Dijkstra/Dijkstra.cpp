83521f667b0e76f146218fe935596abe2ce40f2f
Morticzek <48515330+Morticzek@users.noreply.github.com>
2022-02-11 00:37:04 +0100

    Add files via upload

---
+++ Dijkstra/Dijkstra.cpp
@@ -0,0 +1,10 @@
+﻿// Dijkstra.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include "Dijkstra.h"
+
+int main()
+{
+    loadData((char*)"C:\\Users\\Mortik\\source\\repos\\APL\\x64\\Debug\\test.txt", (char*)"Bydgoszcz");
+}
